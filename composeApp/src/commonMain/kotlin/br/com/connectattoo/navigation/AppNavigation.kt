a02a3b3b00c41da8ce61f013b668e87faf4ac7f2
Antonio Jorge Garcia Tranin <antoniotraninjorge@gmail.com>
2024-11-07 15:22:40 -0400

    feat: update custom theme

--- composeApp/src/commonMain/kotlin/br/com/connectattoo/navigation/AppNavigation.kt
+++ composeApp/src/commonMain/kotlin/br/com/connectattoo/navigation/AppNavigation.kt
@@ -1,15 +1,14 @@
 package br.com.connectattoo.navigation
 
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import br.com.connectattoo.theme.ConnectattooTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
+    ConnectattooTheme {
         Surface {
             AccountManager()
         }
